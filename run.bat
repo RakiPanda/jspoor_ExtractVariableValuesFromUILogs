a8e5b01c0b8faffb6da6fbab9795aaa0741c917b
RakiPanda <scrumhalf0415@gmail.com>
2024-11-26 14:33:11 +0900

    run.bat_修正_TerpPaintが起動

--- run.bat
+++ run.bat
@@ -1,3 +1,5 @@
-SET CLASSPATH=.;./terppaint.jar;./jspoor.jar;
+SET JAVA_HOME=C:\Program Files\Java\jdk-1.8
+SET PATH=%JAVA_HOME%\bin;%PATH%
+SET CLASSPATH=.;%~dp0dist\terppaint.jar;%~dp0dist\jspoor.jar;
 SET ASPECTPATH=./jspoor.jar;
-aj5 jspoor.uilog.UILogApplicationStarter paint.TerpPaint
+aj5 jspoor.uilog.UILogApplicationStarter TerpPaint
